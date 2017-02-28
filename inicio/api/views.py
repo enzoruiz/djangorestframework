b9a752510f4ed0c8c21cdb1413a2c79905ef5374
enzo ruiz <enzo.rp.90@gmail.com>
2017-02-28 16:18:12 -0500

    permisos

--- inicio/api/views.py
+++ inicio/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView
 from .serializers import PeliculaListSerializer, PeliculaRetrieveSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from inicio.models import Pelicula
 
 class PeliculaDetailAPIView(RetrieveAPIView):
@@ -17,3 +18,4 @@ class PeliculaDeleteAPIView(DestroyAPIView):
 class PeliculaListAPIView(ListAPIView):
 	queryset = Pelicula.objects.all()
 	serializer_class = PeliculaListSerializer
+	permission_classes = [IsAuthenticated, ]
